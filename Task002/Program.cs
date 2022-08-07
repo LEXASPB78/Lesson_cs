1295221957f01d1884a0deb6044eff47e9650d29
LEXASPB78 <ashkreba@yandex.ru>
2022-08-07 21:35:35 +0300

    wwwe

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = number % 10;
+System.Console.WriteLine($"Последняя цифра равна {digit}");
+
