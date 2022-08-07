c4655b9375f10fd3735759db11a68426da9ac7d4
LEXASPB78 <ashkreba@yandex.ru>
2022-08-07 21:41:00 +0300

    wwwe

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,5 +1,11 @@
 ﻿Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number < 1000)
+{
 int digit = number % 10;
 System.Console.WriteLine($"Последняя цифра равна {digit}");
-
+}
+else 
+{
+    System.Console.WriteLine("Введеное число не трехзначное");
+}
