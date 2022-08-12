c6fae140a05b0ae7e05128546fae227bf6abdd99
LEXASPB78 <ashkreba@yandex.ru>
2022-08-12 23:23:48 +0300

    Reshenie

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,4 +1,9 @@
-﻿int number = new Random().Next(100, 1000); // [100,999] == [100,1000)
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000); // [100,999] == [100,1000)
 System.Console.WriteLine(number);
 string str_number = number.ToString();
 char digit1 = str_number [0];
