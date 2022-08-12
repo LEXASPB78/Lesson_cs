feb23de424c866a5ff8761bfa63cfd93524dd8d3
LEXASPB78 <ashkreba@yandex.ru>
2022-08-12 21:22:01 +0300

    Reshenie1

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,4 +1,9 @@
-﻿int number = new Random().Next(10,100); // [10,99] == [10,100)
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 8 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+int number = new Random().Next(10,100); // [10,99] == [10,100)
 System.Console.WriteLine(number);
 int digit1 = number / 10; // 74 / 10 = 7 (int)
 int digit2 = number % 10; // 74 % 10 = 4 
