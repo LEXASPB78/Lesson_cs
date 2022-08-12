3b53df1b5115b97080f423e306fb68ef1bd1ae0c
LEXASPB78 <ashkreba@yandex.ru>
2022-08-12 21:10:34 +0300

    Reshenie

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(10,100); // [10,99] == [10,100)
+int digit1 = number / 10; // 74 / 10 = 7 (int)
+int digit2 = number % 10; // 74 % 10 = 4 
+if (digit1 > digit2)
+    System.Console.WriteLine(digit1);
+else 
+    System.Console.WriteLine(digit2);
