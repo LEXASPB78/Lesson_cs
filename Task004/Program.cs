b5f30a73dff7c7a16e6b83a761d6878a69288967
LEXASPB78 <ashkreba@yandex.ru>
2022-08-12 23:22:02 +0300

    Reshenie

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(100, 1000); // [100,999] == [100,1000)
+System.Console.WriteLine(number);
+string str_number = number.ToString();
+char digit1 = str_number [0];
+char digit3 = str_number [2];
+string  new_str_number = digit1.ToString()+digit3.ToString();
+int  new_number = Convert.ToInt32(new_str_number);
+System.Console.WriteLine(new_number);
