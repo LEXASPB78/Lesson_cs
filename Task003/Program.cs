025664cefe4aadf81c1084bfc27fec21bbcb6d7f
LEXASPB78 <ashkreba@yandex.ru>
2022-08-12 21:13:23 +0300

    Reshenie1

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,4 +1,5 @@
 ﻿int number = new Random().Next(10,100); // [10,99] == [10,100)
+System.Console.WriteLine(number);
 int digit1 = number / 10; // 74 / 10 = 7 (int)
 int digit2 = number % 10; // 74 % 10 = 4 
 if (digit1 > digit2)
