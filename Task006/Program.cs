5b503e59d4240cf730b7573161a3316557430deb
LEXASPB78 <ashkreba@yandex.ru>
2022-08-18 22:17:33 +0300

    Reshenie24

---
+++ Task006/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int Sum(int a)
+{
+    int result = 0;
+    for (int number = 0; number <= a; number++)
+    {
+        result += number;
+    }
+    return result;
+}
+
+System.Console.WriteLine(Sum(5));
