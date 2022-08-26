4d0e507c682adba15293ec91b6a8857869e02b7f
LEXASPB78 <ashkreba@yandex.ru>
2022-08-26 21:58:40 +0300

    Reshenie46

---
+++ Task010/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+
+int[] CreateArrey(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for(int i = 0; i<length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i<array.Length; i++)
+        Console.Write($"{array[i]} ");
+
+    Console.WriteLine();
+}
+
+//-----------------------------------------------------------
+
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] array = new int[rowCount, columnCount];
+    for(int row = 0; row < rowCount; ++row)
+    {
+        for(int column = 0; column < columnCount; ++column)
+        {
+            array[row, column] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for(int row = 0; row < arr.GetLength(0); ++row)
+    {
+        for(int column = 0; column < arr.GetLength(1); ++column)
+        {
+            System.Console.Write($"{arr[row, column]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(14, 9, 0, 9999);
+PrintMatrix(matrix);
